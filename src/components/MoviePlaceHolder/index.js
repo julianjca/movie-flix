98a75833f253eeaebdbda7c3f508fb4f407aff15
julianjca <julianjca11@gmail.com>
2019-02-15 12:55:30 +0700

    mapping component

--- src/components/MoviePlaceHolder/index.js
+++ src/components/MoviePlaceHolder/index.js
@@ -7,17 +7,24 @@ const MyLoader = () => (
   </ContentLoader>
 )
 
+const PlaceHolderList = [
+  'pc1',
+  'pc2',
+  'pc3',
+  'pc4',
+  'pc5',
+  'pc6',
+  'pc7',
+  'pc8',
+  'pc9',
+]
 const MoviePlaceHolder = () => (
   <React.Fragment>
-    <MyLoader />
-    <MyLoader />
-    <MyLoader />
-    <MyLoader />
-    <MyLoader />
-    <MyLoader />
-    <MyLoader />
-    <MyLoader />
-    <MyLoader />
+    {
+      PlaceHolderList.map((item)=>{
+        return <MyLoader key={item} />
+      })
+    }
   </React.Fragment>
 )
 
