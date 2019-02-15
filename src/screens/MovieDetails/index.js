bf6203005415b1cc55e7993c79024a5bea14710d
julianjca <julianjca11@gmail.com>
2019-02-15 13:29:57 +0700

    adding store state for movies owned

--- src/screens/MovieDetails/index.js
+++ src/screens/MovieDetails/index.js
@@ -1,13 +1,16 @@
 import React, { Component, Fragment } from 'react';
 import axios from 'axios';
+import { connect } from 'react-redux';
 
 import { MOVIE_BY_ID_API, paramsByID, IMAGE_URL } from '../../constants';
 import PriceHelper from '../../helpers/price';
+import addMovieAction from '../../store/movies/addMovie';
 
 class MovieDetails extends Component {
   state = {
     movieDetails: {},
-    isLoaded: false
+    isLoaded: false,
+    isOwned: false
   }
 
   componentDidMount = async () => {
@@ -19,18 +22,41 @@ class MovieDetails extends Component {
         url: `${MOVIE_BY_ID_API}${movieID}`,
         params: paramsByID
       });
-      console.log(data)
       this.setState({
         isLoaded: true,
-        movieDetails: data
+        movieDetails: data,
+        isBought: this.props.movieData.ownedMovies.find((item)=> {
+          if(item.id = movieID){
+            return true
+          }
+          else {
+            return false
+          }
+        })
       })
     } catch (e) {
       console.log(e)
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    const { movieDetails } = this.state;
+    if(nextProps.movieData.ownedMovies !== this.props.movieData.ownedMovies){
+      this.setState({
+        isOwned: nextProps.movieData.ownedMovies.includes(movieDetails.id.toString())
+      })
+    }
+  }
+
+  addMovie = () => {
+    const { movieDetails } = this.state;
+    if(this.props.movieData.balance - PriceHelper(movieDetails.vote_average)> 0) {
+      this.props.addMovie(movieDetails.id.toString(), PriceHelper(movieDetails.vote_average));
+    }
+  }
+
   render() {
-    const { isLoaded, movieDetails } = this.state;
+    const { isLoaded, movieDetails, isOwned } = this.state;
     return (
       <div>
         {
@@ -42,6 +68,12 @@ class MovieDetails extends Component {
             <h3>Rating {movieDetails.vote_average}</h3>
             <h3>Rp { PriceHelper(movieDetails.vote_average) }</h3>
             <h3>Duration { movieDetails.runtime } minutes</h3>
+            {
+              isOwned ?
+              <h3>you already bought this movie</h3>
+              :
+              <button onClick={this.addMovie}>buy movie</button>
+            }
 
           </Fragment>
           : <h4>Loading Data from API</h4>
@@ -51,4 +83,18 @@ class MovieDetails extends Component {
   }
 }
 
-export default MovieDetails;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    movieData: state
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    addMovie: (movieID, price) => {
+      dispatch(addMovieAction(movieID, price))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieDetails);
