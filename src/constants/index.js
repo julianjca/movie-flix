daa36646e46f8310bb49635a424cd661474ad8a5
julianjca <julianjca11@gmail.com>
2019-02-15 09:47:08 +0700

    fetching movie details from API

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,11 +2,16 @@ const API_KEY = '17f155cf401439f44476ce31a909de90'
 const region = 'ID';
 const language = 'en';
 
-export const params = {
-  API_KEY,
+export const paramsNowPlaying = {
+  api_key: API_KEY,
   region,
   language
 }
 
-export const API_URL = `https://api.themoviedb.org/3/movie/now_playing`;
+export const paramsByID = {
+  api_key: API_KEY
+}
 
+export const NOW_PLAYING_API = 'https://api.themoviedb.org/3/movie/now_playing';
+
+export const MOVIE_BY_ID_API = 'https://api.themoviedb.org/3/movie/'
--- src/screens/MovieDetails.js
+++ src/screens/MovieDetails.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const MovieList = (props) => {
-  const { match : { params } } = props;
-  console.log(params)
-  return (
-    <div>
-      <h2>Movie Details Component</h2>
-    </div>
-  )
+import { MOVIE_BY_ID_API, paramsByID } from '../constants';
+
+class MovieDetails extends Component {
+  state = {
+    movieDetails: {},
+    isLoaded: false
+  }
+
+  componentDidMount = async () => {
+    const { match : { params } } = this.props;
+    const movieID = params.movie.split('-')[0];
+    try {
+      const { data } = await axios({
+        method:'GET',
+        url: `${MOVIE_BY_ID_API}${movieID}`,
+        params: paramsByID
+      });
+      this.setState({
+        isLoaded: true,
+        movieDetails: data
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  render() {
+    const { isLoaded, movieDetails } = this.state;
+    return (
+      <div>
+        <h2>Movie Details Component</h2>
+        {
+          isLoaded
+          ? <h4>{ JSON.stringify(movieDetails) }</h4>
+          : <h4>Loading Data from API</h4>
+        }
+      </div>
+    );
+  }
 }
 
-export default MovieList;
+export default MovieDetails;
