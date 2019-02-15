212434a1a4044837d26cc67dc555eb28d151ce52
julianjca <julianjca11@gmail.com>
2019-02-15 08:39:36 +0700

    adding constants

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+const API_KEY = '17f155cf401439f44476ce31a909de90'
+const region = 'ID';
+const language = 'en';
+
+export const params = {
+  API_KEY,
+  region,
+  language
+}
+
+export const API_URL = `https://api.themoviedb.org/3/movie/now_playing`;
+
