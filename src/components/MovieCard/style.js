cdbc885ee3581de9012cca60e7caaecb4daaaa60
julianjca <julianjca11@gmail.com>
2019-02-15 12:56:46 +0700

    make grid 5 per row

--- src/components/MovieCard/style.js
+++ src/components/MovieCard/style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Div = styled.div`
 text-decoration: none;
 color: black;
-width: 70%;
+width: 90%;
 margin: 0 auto;
 border: 1px solid #1a1a1a;
 border-radius: 10px;
--- src/screens/MovieList/style.js
+++ src/screens/MovieList/style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const GridContainer = styled.div`
 display: grid;
-grid-template-columns: 1fr 1fr 1fr;
+grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
 grid-gap: 20px;
 width: 100%;
   svg {
